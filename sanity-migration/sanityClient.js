5013f00247ebd976b9a98cb423bb13061137e400
Abeersk <Shayanshaikh4112@gmail.com>
2025-02-08 05:08:21 +0530

    Updated repo

--- sanity-migration/sanityClient.js
+++ sanity-migration/sanityClient.js
@@ -11,7 +11,7 @@ dotenv_1.default.config();
 exports.client = (0, client_1.createClient)({
     projectId: process.env.projectId, // Replace with your project ID
     dataset: 'production', // Or your dataset name
-    apiVersion: '2024-01-04', // Today's date or latest API version
+    apiVersion: '2025-01-19', // Today's date or latest API version
     useCdn: false, // Disable CDN for real-time updates
     token: process.env.token,
 });
--- sanity-migration/sanityClient.ts
+++ sanity-migration/sanityClient.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const client = createClient({
   projectId: process.env.projectId, // Replace with your project ID
   dataset: 'production',        // Or your dataset name
-  apiVersion: '2024-01-04',     // Today's date or latest API version
+  apiVersion: '2025-01-19',     // Today's date or latest API version
   useCdn: false,                // Disable CDN for real-time updates
   token: process.env.token,
 });
